835cfe2e356791ef3ce90254470a4ba8800c7b85
Sebastian Schumacher <sebastian.schumacher@hotmail.de>
2021-04-13 10:22:11 +0200

    about text

--- src/contents/About.js
+++ src/contents/About.js
@@ -7,7 +7,7 @@ function About() {
     
     <div className="header">
       <Header title={"About"} />
-      <p>This is my first single page web application built with React. It is fully responsive and fetches comments from the jsonplaceholder fake API which are organized by pagination.</p>
+      <p>Hi! This is my first project realized with React. A fully responsive single page web application fetching comments from the jsonplaceholder fake API which are organized by pagination.</p>
       <p>If you want to see more of my work visit my <a href="https://github.com/sscumac">github</a>.</p>
     </div>
 
