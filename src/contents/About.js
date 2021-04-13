6ef65e2d2ca023457797f3c9e529861fa8b7b7e2
Sebastian Schumacher <sebastian.schumacher@hotmail.de>
2021-04-13 10:19:00 +0200

    pagination position improved

--- src/contents/About.js
+++ src/contents/About.js
@@ -7,8 +7,8 @@ function About() {
     
     <div className="header">
       <Header title={"About"} />
-      <p>This is my first single page web application built with React. It is fully responsive and fetches comments from the jsonplaceholder fake API.</p>
-      <p>If you want to see more of my work vitit my <a href="https://github.com/sscumac">github</a>.</p>
+      <p>This is my first single page web application built with React. It is fully responsive and fetches comments from the jsonplaceholder fake API which are organized by pagination.</p>
+      <p>If you want to see more of my work visit my <a href="https://github.com/sscumac">github</a>.</p>
     </div>
 
   )
